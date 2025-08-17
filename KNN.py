b2ad317d64e71e0a69cf14cc56fa5aa5f6a201b6
marynavaroli <67209505+marynavaroli@users.noreply.github.com>
2025-08-17 15:10:43 -0400

    Remove unused imports

--- KNN.py
+++ KNN.py
@@ -1,17 +1,12 @@
-import sys
 import os
 import math
 from PIL import Image
 import pandas as pd
 import numpy as np
-from sklearn.linear_model import LogisticRegression
-from mlxtend.feature_selection import SequentialFeatureSelector as SFS
-from mlxtend.plotting import plot_sequential_feature_selection as plot_sfs
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.feature_selection import RFE
 from skimage import io
-from sklearn.decomposition import PCA
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
