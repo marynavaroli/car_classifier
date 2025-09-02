a3c60dc9831e9423daba5654fa465aa18902a4aa
marynavaroli <67209505+marynavaroli@users.noreply.github.com>
2025-09-02 00:57:13 -0400

    Revert "Add files via upload"
    This reverts commit f70aa1e1c90c46c6e2bdcd2b0845b582d2cdd9a6.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from PIL import Image
+from KNN import classify_image
 from BaggingRegressor import get_prediction
 from CNN import get_cnn_prediction
 from flask_cors import CORS
@@ -7,6 +8,38 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}}, supports_credentials=True)
 
+@app.route("/classify", methods=["POST", "OPTIONS"])
+def classify():
+    try:
+        if request.method == "OPTIONS":
+            # Preflight request
+            return jsonify({"status": "ok"}), 200
+
+        print("FILES:", request.files)
+        print("FORM:", request.form)
+        if "image" not in request.files:
+            return jsonify({"error": "Missing 'image' in form-data"}), 400
+
+        file = request.files["image"]
+
+        if file.filename == "":
+            return jsonify({"error": "Empty filename"}), 400
+
+        # Load image with Pillow
+        img = Image.open(file.stream)
+
+        # Dummy classification logic (replace with ML model)
+        class_str = classify_image(img)
+
+        return jsonify({
+            "filename": file.filename,
+            "class": class_str
+        })
+
+    except Exception as e:
+        import traceback
+        traceback.print_exc() 
+        return jsonify({"error": str(e)}), 500
 
 @app.route("/predict", methods=["POST", "OPTIONS"])
 def predict():
