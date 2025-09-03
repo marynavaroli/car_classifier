919d24841cc98a2a2df308cee5ae84466106eed7
Mary <67209505+marynavaroli@users.noreply.github.com>
2025-09-03 02:16:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from PIL import Image
 from BaggingRegressor import get_prediction
-from CNN import get_cnn_prediction
+
 from KNN import classify_image
 from flask_cors import CORS
 
@@ -82,41 +82,6 @@ def predict():
         traceback.print_exc()
         return jsonify({"error": str(e)}), 500
     
-
-@app.route("/cnn", methods=["POST", "OPTIONS"])
-def cnn():
-    try:
-        print("CNN route hit") 
-        if request.method == "OPTIONS":
-            # Preflight request
-            return jsonify({"status": "ok"}), 200
-
-        print("FILES:", request.files)
-        print("FORM:", request.form)
-        if "image" not in request.files:
-            return jsonify({"error": "Missing 'image' in form-data"}), 400
-
-        file = request.files["image"]
-
-        if file.filename == "":
-            return jsonify({"error": "Empty filename"}), 400
-
-        # Load image with Pillow
-        img = Image.open(file.stream)
-        print("image opened") 
-
-        # Dummy classification logic (replace with ML model)
-        class_str = get_cnn_prediction(img)
-        print("get called")
-        return jsonify({
-            "filename": file.filename,
-            "class": class_str
-        })
-
-    except Exception as e:
-        import traceback
-        traceback.print_exc() 
-        return jsonify({"error": str(e)}), 500
 
 
 if __name__ == "__main__":
